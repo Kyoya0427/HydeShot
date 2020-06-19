c0097708d2adb9e948ba833a41415a4102c35785
Kyoya0427 <47050551+Kyoya0427@users.noreply.github.com>
2020-06-19 14:40:11 +0900

    Add files via upload

---
+++ Game/Collider/RayCollider.h
@@ -0,0 +1,37 @@
+//======================================================
+// File Name	: RayCollider.h
+// Summary		: 箱
+// Date			: 2020/5/12
+// Author		: Kyoya Sakamoto
+//======================================================
+#pragma once
+
+#include "Collider.h"
+
+class RayCollider : public Collider
+{
+public:
+	//コンストラクタ
+	RayCollider(GameObject* owner);
+	//デストラクタ
+	~RayCollider() = default;
+
+public:
+	//当たったオブジェクトの種類判別
+	bool IsCollided(const Collider*       collider) const override;
+	bool IsCollided(const SphereCollider* collider) const override;
+	bool IsCollided(const BoxCollider*    collider)	const override;
+	bool IsCollided(const RayCollider*    collider) const override;
+
+public:
+	DirectX::SimpleMath::Vector3 GetPosA() const;
+	DirectX::SimpleMath::Vector3 GetPosB() const;
+	//サイズの取得と設定
+	void SetPosA(DirectX::SimpleMath::Vector3 posA);
+	void SetPosB(DirectX::SimpleMath::Vector3 posB);
+
+
+private:
+	DirectX::SimpleMath::Vector3 m_posA;
+	DirectX::SimpleMath::Vector3 m_posB;
+};
---
+++ Game/GameObject/Sight.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <DirectXTK/PrimitiveBatch.h>
+#include <Game/GameObject/GameObject.h>
+
+#include <Game/Collider/RayCollider.h>
+
+class Character;
+
+class Sight : public GameObject
+{
+public:
+	Sight(Character* chara);
+	~Sight();
+
+public:
+	// XV
+	void Update(const DX::StepTimer& timer) override;
+	// •`‰æ
+	void Render() override;
+	//“–‚½‚Á‚½Œã‚Ìˆ—
+	void OnCollision(GameObject* object) override;
+
+private:
+	std::unique_ptr<DirectX::GeometricPrimitive> m_sightCollider;
+	std::unique_ptr<DirectX::PrimitiveBatch<DirectX::VertexPositionColor>>  m_lineCollider;
+	std::unique_ptr<DirectX::BasicEffect> m_basicEffect;
+	//“–‚½‚è”»’è
+	std::unique_ptr<RayCollider>				 m_collider;
+	DirectX::SimpleMath::Vector3                 m_size;
+	DirectX::SimpleMath::Vector3                 m_posA;
+	DirectX::SimpleMath::Vector3                 m_posB;
+
+	Character*                                   m_chara;
+	bool                                         m_contact;
+};
+
