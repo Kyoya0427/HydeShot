4cde665802873c40027c735978933d9f3baa7462
Kyoya0427 <47050551+Kyoya0427@users.noreply.github.com>
2020-06-25 20:09:15 +0900

    Add files via upload

---
+++ Game/AI/RuleBased.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "RuleBased.h"
+
+RuleBased::RuleBased()
+{
+}
+
+RuleBased::~RuleBased()
+{
+}
+
+AIController::Behavior RuleBased::BehaviorSelection(Character* character)
+{
+    return AIController::Behavior();
+}
---
+++ Game/AI/RuleBased.h
@@ -0,0 +1,24 @@
+//======================================================
+// File Name	: RuleBased.h
+// Summary		: ニューラルネットワーク
+// Date			: 2020/6/12
+// Author		: Kyoya  Sakamoto
+//======================================================
+#pragma once
+
+#include <Game/Controller/AIController.h>
+
+class Character;
+
+class RuleBased
+{
+public:
+	RuleBased();
+	~RuleBased();
+
+public:
+	AIController::Behavior BehaviorSelection(Character* character);
+	 
+
+};
+
