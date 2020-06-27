771e6d33e5a9a466e6cb197e2be98f9894829cdc
Kyoya0427 <47050551+Kyoya0427@users.noreply.github.com>
2020-06-27 22:18:05 +0900

    Add files via upload

---
+++ Game/Controller/NeuralNetworkManager.h
@@ -0,0 +1,36 @@
+//======================================================
+// File Name	: NeuralNetworkManager.h
+// Summary		: ÉÇÅ[ÉhëIë
+// Date			: 2020/6/9
+// Author		: Kyoya  Sakamoto
+//======================================================
+#pragma once
+
+#include <vector>
+
+#include <Game/Controller/AIController.h>
+
+class NeuralNetwork;
+
+class NeuralNetworkManager
+{
+public:
+	NeuralNetworkManager();
+	~NeuralNetworkManager();
+
+public:
+	static const int MAX_DATA_H = 3920; 
+	static const int MAX_DATA_W = 4; 
+
+public:
+	void InitializeTraining(wchar_t* fname);
+	void InitializeNeuralNetwork();
+	AIController::Behavior BehaviorSelection(float x, float y, int hp);
+	void Render();
+public: 
+
+	std::vector<std::vector<double>> m_training;
+	double                           m_error;
+	std::unique_ptr<NeuralNetwork>   m_neuralNetwork;
+};
+
