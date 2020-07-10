61b3ff6ceae4ff0557d669ae645491e5870e1c1c
Kyoya0427 <47050551+Kyoya0427@users.noreply.github.com>
2020-07-10 15:27:32 +0900

    Add files via upload

---
+++ Game/AI/NeuralNetworkManager.h
@@ -0,0 +1,43 @@
+//======================================================
+// File Name	: NeuralNetworkManager.h
+// Summary		: ニューラルネットワークでモード選択
+// Date			: 2020/6/9
+// Author		: Kyoya  Sakamoto
+//======================================================
+#pragma once
+
+#include <vector>
+
+#include <Game/Controller/AIController.h>
+
+class NeuralNetwork;
+class Character;
+
+class NeuralNetworkManager
+{
+public:
+	//コンストラクタ
+	NeuralNetworkManager();
+	//デストラクタ
+	~NeuralNetworkManager();
+
+public:
+	static const int MAX_DATA_H = 3920; 
+	static const int MAX_DATA_W = 4; 
+//	static const int MAX_DATA_H = 301;
+//	static const int MAX_DATA_W = 7;
+public:
+	//データを取得
+	void InitializeTraining(wchar_t* fname);
+	//ニューラルネットワークの初期化
+	void InitializeNeuralNetwork();
+	//行動パターンを選択
+	AIController::Behavior BehaviorSelection(Character* character, Character* enemys);
+
+public: 
+	//データを格納
+	std::vector<std::vector<double>> m_training;
+	//ニューラルネットワーク
+	std::unique_ptr<NeuralNetwork>   m_neuralNetwork;
+};
+
